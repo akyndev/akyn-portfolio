bbf2c6a802620d73fb3892d57d75f9337f10da3e
Joseph Akinlade <akinaldeirede@gmail.com>
2024-07-10 01:29:54 +0100

    opengraph-image-alt.txt & twitter-image-alt.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,96 +1,35 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { siteConfig } from "@/seo";
 import type { Metadata } from "next";
-import { siteConfig } from "../seo";
 import { general_sans } from "./font";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: siteConfig.name,
+  title: siteConfig.title,
   description: siteConfig.description,
-  generator: "Akyn",
-  applicationName: "Akyn portfolio",
+  generator: siteConfig.owner,
+  applicationName: siteConfig.name,
   referrer: "origin-when-cross-origin",
   keywords: siteConfig.keywords,
-  authors: [{ name: siteConfig.owner, url: siteConfig.url }],
   creator: siteConfig.owner,
   publisher: siteConfig.owner,
-
-  icons: {
-    icon: [
-      { url: "/icon.png" },
-      new URL("/icon.png", "https://example.com"),
-      { url: "/icon-dark.png", media: "(prefers-color-scheme: dark)" },
-    ],
-    shortcut: ["/shortcut-icon.png"],
-    apple: [
-      { url: "/apple-icon.png" },
-      { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },
-    ],
-    other: [
-      {
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
-      },
-    ],
-  },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
+  authors: [{ name: siteConfig.owner, url: siteConfig.url }],
   openGraph: {
-    title: siteConfig.name,
+    title: siteConfig.title,
     description: siteConfig.description,
     url: siteConfig.url,
-    siteName: "Akyn",
-    images: [
-      {
-        url: "https://nextjs.org/og.png", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
+    siteName: siteConfig.name,
     locale: "en_US",
     type: "website",
   },
   twitter: {
-    card: "app",
-    title: siteConfig.name,
+    card: "summary_large_image",
+    title: siteConfig.title,
     description: siteConfig.description,
     siteId: "1463402446688972801",
     creator: "@akyndev",
     creatorId: "1463402446688972801",
-    images: {
-      url: "https://nextjs.org/og.png",
-      alt: "Next.js Logo",
-    },
-    app: {
-      name: "twitter_app",
-      id: {
-        iphone: "twitter_app://iphone",
-        ipad: "twitter_app://ipad",
-        googleplay: "twitter_app://googleplay",
-      },
-      url: {
-        iphone: "https://iphone_url",
-        ipad: "https://ipad_url",
-      },
-    },
   },
 };
 
--- src/seo.ts
+++ src/seo.ts
@@ -1,10 +1,18 @@
-
-
-
 export const siteConfig = {
-    name: "Akyn"            ,
-    description: "Web developer skilled in Framer, Next.js, React, TypeScript, and JavaScript. Specializing in dynamic, high-performance web applications with a focus on interactive design and seamless user experiences.",
-    url: "https://akyn.dev",
-    keywords: ["Next.js", "React", "JavaScript", "framer"],
-    owner: "Akyndev"
-}
+  title: "Akyn | Web Developer",
+  description:
+    "Web developer skilled in Framer, Next.js, React, TypeScript, and JavaScript. Specializing in dynamic, high-performance web applications with a focus on interactive design and seamless user experiences.",
+  url: "https://akyn.dev",
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Framer",
+    "Webflow",
+    "HTML",
+    "CSS",
+    "Typescript",
+  ],
+  owner: "Akyndev",
+  name: "Akyn Portfolio",
+};
